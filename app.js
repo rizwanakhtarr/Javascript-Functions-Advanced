c9788841f5d0d029e67914dae0e56127cb2bfd62
rizwanakhtarr <you@example.com>
2018-05-29 15:09:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// 1. Write a function called 'multiply' that multiplies two numbers and returns the result
+function multiply () {
+
+}
+
+// 2. Write a function called 'addThree' that adds three numbers together and returns the result
+
+// 3. Write a function called 'smallestNumber' that returns the smaller of 2 numbers
+
+// 4. Write a function called 'maxOfThree' that returns the largest of 3 numbers
+
+// 5. Write a function called 'reverseString' that returns the reverse a string 
+
+// 6. Write a function called 'disemvowel' that returns the vowels from the string parameter
+
+// 7. Write a function called 'removeOdd' that removes all ODD number from an array
+
+// 8. Write a function called 'removeEven' that removes all EVEN number from an array
+
+// 9. Write a function called 'longestString' that takes an array of strings and returns the string with the longest character length
+
+// 10. Write a function called 'allElementsExceptFirstThree' that discards the first 3 elements of an array, 
+// e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
+
+//#### BONUS ####
+
+// 11. Write a function called 'convertArrayToAnObject' that turns an array (with an even number of elements) into a hash, by
+// pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
+// {'a' => 'b', 'c' => 'd'}
+
+// 12. Write a function called 'fizzBuzz' that takes any number and returns a value based on these rules 
+
+// But for multiples of three print "Fizz" instead of the number 
+// For the multiples of five print "Buzz". 
+// For numbers which are multiples of both three and five print "FizzBuzz".
+
